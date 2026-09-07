b0d9a6ef831f8c4e0ab3a248ab1919e6204d70ed
louisophie <louisophie@gmail.com>
2026-09-07 21:21:23 +0800

    android

--- bloodsugar_meal-photo.pl
+++ bloodsugar_meal-photo.pl
@@ -7,22 +7,21 @@ use File::Basename qw(basename);
 my$file="bloodsugar.md";
 #my$phone_img_folder="/home/louisophie/Downloads/github/BG_record/image.bak";
 my$phone_img_folder="/data/data/com.termux/files/home/storage/shared/DCIM/Camera";
-#<magic begin
 @_ = glob("${phone_img_folder}/*.jpg");
 $_ = (sort { -M $a <=> -M $b } @_)[0];
 my$img_name=basename($_) ;
 #system("jpegoptim", "-v", "-d", "./image/", "--size=100k", "--strip-all", "-o", "$_") == 0 or die "jpegoptim failed: $?";
-system("magick", "$_", "-resize", "20%", "-strip", "-quality", "80", "-monitor", "./image/$img_name") == 0 or die "magick failed: $?";
+system("magick", "$_", "-resize", "100%", "-strip", "-quality", "80", "-monitor", "./image/$img_name") == 0 or die "magick failed: $?";
 print "proceed? [Enter=yes, Ctrl-C=abort] ";<STDIN>;
 #>magic end
 
-#<find the line-number begin
+#<the line-number finding begin
 my $line;
 open my $fh, '<', "$file" or die "Cannot open $file: $!";
 while(<$fh>){
-	if(/^###/ || /^\d+\. \d\d:/ || /^ +?- +?`\{/){
+	#if(/^###/ || /^\d+\. \d\d:/ || /^ +?- +?`\{/){
+	unless(/^$/ || /^-+$/ || /^ *$/){
 			$line = $.;
-			#END
 		}
 }
 close $fh;
@@ -30,12 +29,12 @@ close $fh;
 
 #<insert an img to the bloodsugar.md with .bak
 if (defined $line) {
-    local $^I = ".bak";          # backup; set to "" for no backup
-    local @ARGV = ($file);
-    while(<>){
-        s/$/ ![${img_name}](.\/image\/${img_name})/ if $. == $line;
-        print;
-    }
+	local $^I = ".bak";          # backup; set to "" for no backup
+	local @ARGV = ($file);
+	while(<>){
+		s/$/ ![${img_name}](.\/image\/${img_name})/ if $. == $line;
+		print;
+	}
 }
 system("cat", "-n", "$file");
 
